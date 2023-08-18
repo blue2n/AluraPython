c7549bb2530a0efc2678f45c508481db0355a65d
Blue <101784753+blue2n@users.noreply.github.com>
2023-08-18 16:18:59 -0300

    Add files via upload

---
+++ jogos.py
@@ -0,0 +1,21 @@
+import adivinhacao
+import forca
+
+def escolher_jogo():
+    print('-----------------------------------')
+    print('----Bem vindo ao jogo de Forca!----')
+    print('-----------------------------------')
+
+    print('(1) Forca (2) Adivinhação')
+
+    jogo = int(input('Qual o jogo? '))
+
+    if (jogo == 1):
+        print('Jogando Forca')
+        forca.jogar()
+    elif (jogo == 2):
+        print('Jogando Adivinhação')
+        adivinhacao.jogar()
+
+if(__name__ == '__main__'):
+    escolher_jogo()
